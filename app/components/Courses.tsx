517d39ecc102ead9e79ecf5028d36c9f0b43b9b6
adairrohling <81537071+adairrohling@users.noreply.github.com>
2023-11-01 01:47:55 +0000

    feat: Cursos acessados pelo banco de dados

--- app/components/Courses.tsx
+++ app/components/Courses.tsx
@@ -1,11 +1,11 @@
-export default function Courses() {
-
-    const courses = [
-        { title: "Curso de HTML", url: "/img/html.svg", description: "O curso é realizado por..." },
-        { title: "Curso de CSS", url: "/img/css.svg", description: "O curso CSS é realizado por..." },
-        { title: "Curso de JS", url: "/img/js.svg", description: "O curso JSS é realizado por..." }
-    ]
-
+import { sql } from "@vercel/postgres";
+export default async function Courses() {
+    // const courses = [
+    //     { title: "Curso de HTML", url: "/img/html.svg", description: "O curso é realizado por..." },
+    //     { title: "Curso de CSS", url: "/img/css.svg", description: "O curso CSS é realizado por..." },
+    //     { title: "Curso de JS", url: "/img/js.svg", description: "O curso JSS é realizado por..." }
+    // ]
+    const { rows } = await sql`SELECT * from courses`;
     return (
         <main className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
             <div className="md:col-span-2 lg:col-span-3 mt-4 text-white text-center">
@@ -14,9 +14,9 @@ export default function Courses() {
                 </h2>
             </div>
             {
-                courses.map((course) => {
+                rows.map((course) => {
                     return (
-                        <div className="bg-[#4d4d4d] rounded-md pb-2">
+                        <div key={course.id} className="bg-[#4d4d4d] rounded-md pb-2">
                             <a href="/curso_html.html">
                                 <img className="hover:scale-105" src={course.url} alt="" />
                                 <div className="text-white text-center">
